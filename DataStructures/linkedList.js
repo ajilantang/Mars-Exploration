a18d64020367fd39eedf425e37a5ad7b9506e9f3
ajilantang <ajilantang@gmail.com>
2017-05-27 16:37:52 +0700

    week 7

--- DataStructures/linkedList.js
+++ DataStructures/linkedList.js
@@ -1,15 +1,33 @@
-/* Node is defined as
-var Node = function(data) {
-    this.data = data;
-    this.next = null;
-}
+
+/*
+  Node is defined as
+  var Node = function(data) {
+      this.data = data;
+      this.next = null;
+  }
 */
+
 // This is a "method-only" submission.
 // You only need to complete this method.
 
-function print(head) {
-        while(head !== null){
-            console.log(head.data)
-            head=head.next
+function insert(head, data) {
+    let value = head
+    let tail = null
+    while (true) {
+        if(value === null){
+            return new Node(data)
         }
+        
+        if (value.next === null ) {
+            tail = value
+            break
+        }
+        
+        value = value.next
+    }
+    
+    tail.next = new Node(data)
+    
+    return head
 }
+
